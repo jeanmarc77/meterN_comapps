d2a559d57849ebfe38eeba2155bf3e7e691262fd
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2022-02-21 17:14:29 +0100

    Update peaknlow.php

--- misc examples/peaknlow.php
+++ misc examples/peaknlow.php
@@ -3,9 +3,9 @@
 // A simple script to return peak power and lower power of the last 24h hours.
 // Use as indicator
 
-$MNDIR    = '/srv/http/metern'; // Path to meterN
-$METNUM   = 1; // live meter number to check
-$INDID = 'peak'; // this indicator ID
+$MNDIR  = '/srv/http/metern'; // Path to meterN
+$METNUM = 1; // live meter number to check
+$INDID  = 'peak'; // this indicator ID
 
 // No edit should be needed bellow
 $prevfile = '/dev/shm/prevpeaknlow.json';
@@ -34,33 +34,33 @@ if (file_exists($LIVEMEMORY)) {
     if (isset($argv[1]) && isset($array['UTC'])) {
         if ($argv[1] == '-peak') {
             if ($nowutc - $array['UTC'] < 5 && $array["${'METNAME'.$METNUM}$METNUM"] > $previous['max']) { // peak
-				$previous['max'] = $array["${'METNAME'.$METNUM}$METNUM"];
-				$previous['tmax'] = $nowutc;
+                $previous['max']  = $array["${'METNAME'.$METNUM}$METNUM"];
+                $previous['tmax'] = $nowutc;
             }
             $ret = $previous['max'];
             echo "$INDID($ret*W)";
         } elseif ($argv[1] == '-low') {
             if (($nowutc - $array['UTC'] < 5 && $array["${'METNAME'.$METNUM}$METNUM"] < $previous['low'] && $array["${'METNAME'.$METNUM}$METNUM"] > 0) || !isset($previous['low'])) { // low
-				$previous['low'] = $array["${'METNAME'.$METNUM}$METNUM"];
-				$previous['tlow'] = $nowutc;
+                $previous['low']  = $array["${'METNAME'.$METNUM}$METNUM"];
+                $previous['tlow'] = $nowutc;
             }
             $ret = $previous['low'];
             echo "$INDID($ret*W)";
         }
         /*
-		if (date('H') == 0 && date('i') == 0) { // Clear at midnight
-            $previous['max'] = $array["${'METNAME'.$METNUM}$METNUM"];
-            $previous['low'] = $array["${'METNAME'.$METNUM}$METNUM"];
+        if (date('H') == 0 && date('i') == 0) { // Clear at midnight
+        $previous['max'] = $array["${'METNAME'.$METNUM}$METNUM"];
+        $previous['low'] = $array["${'METNAME'.$METNUM}$METNUM"];
         }*/
-		if ($previous['tmax'] > $nowutc + 86400) { // Clear value over 24h
-			$previous['tmax'] = $nowutc;
-			$previous['max'] = $array["${'METNAME'.$METNUM}$METNUM"];
-		}
-		if ($previous['tlow'] > $nowutc + 86400) {
-			$previous['tlow'] = $nowutc;
-			$previous['low'] = $array["${'METNAME'.$METNUM}$METNUM"];
-		}
-        $prevdata        = json_encode($previous);
+        if ($previous['tmax'] > $nowutc + 86400) { // Clear value over 24h
+            $previous['tmax'] = $nowutc;
+            $previous['max']  = $array["${'METNAME'.$METNUM}$METNUM"];
+        }
+        if ($previous['tlow'] > $nowutc + 86400) {
+            $previous['tlow'] = $nowutc;
+            $previous['low']  = $array["${'METNAME'.$METNUM}$METNUM"];
+        }
+        $prevdata = json_encode($previous);
         file_put_contents($prevfile, $prevdata);
     } else {
         echo "Usage: peaknlow { peak | low }\n";
