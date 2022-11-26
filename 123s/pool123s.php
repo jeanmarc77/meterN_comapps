4e9107b3c1cf4fe7d29ec459576677e30b1dc9ed
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2022-11-26 20:01:53 +0100

    Update pool123s.php
    set null when asleep

--- 123s/pool123s.php
+++ 123s/pool123s.php
@@ -4,7 +4,7 @@ if (isset($_SERVER['REMOTE_ADDR'])) {
     die('Direct access not permitted');
 }
 // This script will output a 123solar counter into a meterN compatible format
-// Configure, then ln -s /srv/http/comapps/pool123s.php /usr/bin/pool123s and chmod +x pool123s.php
+// Configure, then ln -s /srv/http/comapps/123s/pool123s.php /usr/bin/pool123s and chmod +x pool123s.php
 // Request Main command with 'pool123s -energy' and live command 'pool123s -power'
 
 // 123solar config
@@ -14,38 +14,45 @@ $invtnum    = 1;
 $METERID    = 'solar';
 
 // No edit is needed below
-if (isset($argv[1])) {
+if (isset($argv[1]) && ($argv[1] == '-power' || $argv[1] == '-energy')) {
     include("$pathto123s/config/memory.php");
-    
     $KWHT = null;
-    if (file_exists($LIVEMEMORY)) {
-        $data     = file_get_contents($LIVEMEMORY);
+    $GP   = null;
+    
+    if (file_exists($MEMORY)) {
+        $data     = file_get_contents($MEMORY);
         $memarray = json_decode($data, true);
-        $nowUTC   = strtotime(date("Ymd H:i:s"));
-        if ($argv[1] == '-power') {
-            if ($nowUTC - $memarray["SDTE$invtnum"] < 300) {
-                $GP = $memarray["G1P$invtnum"] + $memarray["G2P$invtnum"] + $memarray["G3P$invtnum"];
-                $GP = round($GP, 1);
-            } else { // Too old
-                die("Abording: Too late value\n");
-            }
-            echo "$METERID($GP*W)\n";
-        } elseif ($argv[1] == '-energy') {
-            if ($nowUTC - $memarray["SDTE$invtnum"] < 600) {
-                if (isset($memarray["KWHT$invtnum"])) {
-                    $KWHT = round($memarray["KWHT$invtnum"] * 1000); // Wh
-                    echo "$METERID($KWHT*Wh)\n";
+        
+        if ($memarray['awake'] && file_exists($LIVEMEMORY)) {
+            $data     = file_get_contents($LIVEMEMORY);
+            $memarray = json_decode($data, true);
+            $nowUTC   = strtotime(date("Ymd H:i:s"));
+            if ($argv[1] == '-power') {
+                if ($nowUTC - $memarray["SDTE$invtnum"] < 300) {
+                    $GP = $memarray["G1P$invtnum"] + $memarray["G2P$invtnum"] + $memarray["G3P$invtnum"];
+                    $GP = round($GP, 1);
+                } else { // Too old
+                    die("Abording: Too late value\n");
+                }
+                
+            } elseif ($argv[1] == '-energy') {
+                if ($nowUTC - $memarray["SDTE$invtnum"] < 600) {
+                    if (isset($memarray["KWHT$invtnum"])) {
+                        $KWHT = round($memarray["KWHT$invtnum"] * 1000); // Wh
+                    } else {
+                        die("Abording: KWHT not defined\n");
+                    }
                 } else {
-                die("Abording: KWHT not defined\n");
-		}
-            } else {
-                die("Abording: Too late value\n");
+                    die("Abording: Too late value\n");
+                }
             }
-        } else {
-            die("Abording: no valid argument given\n");
         }
-    } else { // 123s ain't running
-        die("Abording: Empty SHM\n");
+    }
+    if ($argv[1] == '-power') {
+        echo "$METERID($GP*W)\n";
+    } elseif ($argv[1] == '-energy') {
+        echo "$METERID($KWHT*Wh)\n";
+        
     }
 } else {
     die("Usage: pool123s { power | energy }\n");
